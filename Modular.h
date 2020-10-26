6db5f9483349de58b95ad4a3d83b5d09e7a7cbc9
nastya <60066872+nastygorodi@users.noreply.github.com>
2020-10-26 18:50:53 +0500

    Update Modular.h

--- Modular.h
+++ Modular.h
@@ -3,55 +3,63 @@
 #include <optional>
 
 namespace Equations {
-using IntegralType = std::int64_t;
+template <class TIntegralType, class = std::enable_if_t <std::is_integral_v <TIntegralType>>>
+struct GCDResult {
+    TIntegralType gcd;
+    TIntegralType first_coefficient;
+    TIntegralType second_coefficient;
+};
+
+template <class TIntegralType, class = std::enable_if_t <std::is_integral_v <TIntegralType>>>
+struct DiophantineSolution {
+    TIntegralType first_coefficient;
+    TIntegralType second_coefficient;
+};
 
 //solve an equation ax + by = (a, b)
-IntegralType gcd_extended (IntegralType coeff_1, IntegralType coeff_2, IntegralType& x, IntegralType& y) {
-    IntegralType divider, quotient, remainder, x2, x1, y2, y1;
-    if (coeff_2 == 0) {
+template <class TIntegralType, class = std::enable_if_t <std::is_integral_v <TIntegralType>>>
+GCDResult<TIntegralType> gcd_extended (TIntegralType value1, TIntegralType value2) {
+    TIntegralType divider, quotient, remainder, x, x2, x1, y, y2, y1;
+    if (value2 == 0) {
         x = 1;
         y = 0;
-        divider = coeff_1;
-        return divider;
+        divider = value1;
+        return {divider, x, y};
     }
     x2 = 1;
     x1 = 0;
     y2 = 0;
     y1 = 1;
-    while (coeff_2 > 0) {
-        quotient = coeff_1 / coeff_2;
-        remainder = coeff_1 - quotient * coeff_2;
+    while (value2 > 0) {
+        quotient = value1 / value2;
+        remainder = value1 - quotient * value2;
         x = x2 - quotient * x1;
         y = y2 - quotient * y1;
-        coeff_1 = coeff_2;
-        coeff_2 = remainder;
+        value1 = value2;
+        value2 = remainder;
         x2 = x1;
         x1 = x;
         y2 = y1;
         y1 = y;
     }
-    divider = coeff_1;
+    divider = value1;
     x = x2;
     y = y2;
-    return divider;
+    return {divider, x, y};
 }
 
-std::optional<std::pair<IntegralType, IntegralType>> solve_equation (IntegralType coeff_1, IntegralType coeff_2, IntegralType coeff_3, IntegralType& param_1, IntegralType& param_2) {
-    IntegralType divider = gcd_extended(coeff_1, coeff_2, param_1, param_2);
-    //если (a, b) не делит const --> реш-й не сущ.
-    if (coeff_3 % divider != 0) {
-        return {};
-    }
-    //находили для ax + by = (a, b) --> чтобы получить решение диофантова ур-я \cdot const/(a, b)
-    param_1 *= coeff_3 / divider;
-    param_2 *= coeff_3 / divider;
-    return std::pair(param_1, param_2);
+template <class TIntegralType, class = std::enable_if_t <std::is_integral_v <TIntegralType>>>
+DiophantineSolution<TIntegralType> solve_equation (TIntegralType value1, TIntegralType value2, TIntegralType rhs) {
+    DiophantineSolution<TIntegralType> solution;
+    auto result = gcd_extended<TIntegralType>(value1, value2);
+    assert(rhs % result.gcd == 0);
+    solution.first_coefficient = result.first_coefficient * rhs / result.gcd;
+    solution.second_coefficient = result.second_coefficient * rhs / result.gcd;
+    return solution;
 }
 }
 
 namespace Groebner {
-using namespace Equations;
-
 template < std::int64_t mod >
 class Modular {
 static_assert(mod > 0, "The modulus must be positive!");
@@ -94,12 +102,8 @@ public:
     }
 
     Modular& operator/=(const Modular& divider) {
-        assert(std::gcd(divider.number_, mod) == 1);
-        IntegralType param_1, param_2;
-        std::optional<std::pair<IntegralType, IntegralType>> solution = solve_equation(divider.number_, mod, number_, param_1, param_2);
-        if (solution != std::nullopt) {
-            number_ = (*solution).first;
-        }
+        auto solution = Equations::solve_equation<IntegralType>(divider.number_, mod, number_);
+        number_ = solution.first_coefficient;
         reduce();
         return *this;
     }

7f5440b38b1bfff38325226d14327476ec616198
nastya <60066872+nastygorodi@users.noreply.github.com>
2020-11-08 19:45:15 +0300

    Update Order.h

--- Order.h
+++ Order.h
@@ -5,62 +5,71 @@ namespace Groebner {
 class Lex {
 public:
     static bool cmp(const Monomial& first, const Monomial& second) {
-        auto first_degrees = first.get_degrees();
-        auto second_degrees = second.get_degrees();
-        auto it1 = first_degrees.begin();
-        auto it2 = second_degrees.begin();
-        while(it1 != first_degrees.end() && it2 != second_degrees.end() &&
-                             it1->first == it2->first && it1->second == it2->second) {
-            ++it1;
-            ++it2;
+        auto begin1 = first.get_degrees().begin();
+        auto end1 = first.get_degrees().end();
+        auto begin2 = second.get_degrees().begin();
+        auto end2 = second.get_degrees().end();
+        while (begin1 != end1 && begin2 != end2 &&
+                             begin1->first == begin2->first && begin1->second == begin2->second) {
+            ++begin1;
+            ++begin2;
         }
-        if (it1 != first_degrees.end()) {
-            if (it2 != second_degrees.end()) {
-                if (it1->first < it2->first || (it1->first == it2->first && it1->second > it2->second)) {
-                    return true;
-                }
-            } else {
-                return true;
-            }
+        return Lex::compare(begin1, end1, begin2, end2);
+    }
+private:
+    using Iter = std::map<Monomial::IndexType, Monomial::DegreeType>::const_iterator;
+    static bool compare(Iter begin1, Iter end1, Iter begin2, Iter end2) {
+        if (begin1 != end1 && begin2 != end2 && (begin1->first > begin2->first
+                 || (begin1->first == begin2->first && begin1->second < begin2->second))) {
+            return true;
+        }
+        if (begin1 == end1 && begin2 != end2) {
+            return true;
         }
         return false;
-}
+    }
+
 };
 
 class Deg {
 public:
     static bool cmp(const Monomial& first, const Monomial& second) {
-        int64_t deg1 = 0;
-        int64_t deg2 = 0;
-        auto first_degrees = first.get_degrees();
-        auto second_degrees = second.get_degrees();
-        for (auto it1 = first_degrees.begin(); it1 != first_degrees.end(); ++it1) {
-            deg1 += it1->second;
+        return Deg::compare(first.get_degrees().begin(), first.get_degrees().end(),
+                                 second.get_degrees().begin(), second.get_degrees().end());
+    }
+private:
+    using Iter = std::map<Monomial::IndexType, Monomial::DegreeType>::const_iterator;
+    static Monomial::DegreeType total_degree(Iter it1, Iter it2) {
+        Monomial::DegreeType res = 0;
+        while (it1 != it2) {
+            res += it1->second;
+            ++it1;
         }
-        for (auto it2 = second_degrees.begin(); it2 != second_degrees.end(); ++it2) {
-            deg2 += it2->second;
-        }
-        if (deg1 > deg2) {
-            return true;
-        }
-        return false;
+        return res;
+    }
+
+    static bool compare(Iter begin1, Iter end1, Iter begin2, Iter end2) {
+        Monomial::DegreeType deg1 = total_degree(begin1, end1);
+        Monomial::DegreeType deg2 = total_degree(begin2, end2);
+        return (deg1 < deg2);
     }
 };
 
 template <class ...Ts>
 class Sum;
 
-//почему это так работает??????? без ...<T>  в нижней строке ничего не работало
-//я посмотрела в интернете, но мне очень хочется послушать от тебя про эту (заморочку), ой, магию
 template <class T>
 class Sum<T> {
 public:
     static bool cmp(const Monomial& first, const Monomial& second) {
         return T::cmp(first, second);
     }
+    
+    bool operator()(const Monomial& first, const Monomial& second) {
+        return cmp(first, second);
+    }
 };
 
-//тот же вопрос
 template <class T1, class ...Tk>
 class Sum<T1, Tk...> {
 public:
@@ -68,6 +77,14 @@ public:
         if (T1::cmp(first, second)) return true;
         if (T1::cmp(second, first)) return false;
         return Sum<Tk...>::cmp(first, second);
+    }
+};
+
+template <class T>
+class Reverse {
+public:
+    static bool cmp(const Monomial& first, const Monomial& second) {
+        return T::cmp(second, first);
     }
 };
 }

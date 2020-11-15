e5b94e2b4a09e5d2f025b5f64bd2250735888a52
nastya <60066872+nastygorodi@users.noreply.github.com>
2020-11-15 20:45:18 +0300

    Update Order.h

--- Order.h
+++ Order.h
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include "Monomial.h"
 
@@ -5,38 +6,34 @@ namespace Groebner {
 class Lex {
 public:
     static bool cmp(const Monomial& first, const Monomial& second) {
-        auto begin1 = first.get_degrees().begin();
-        auto end1 = first.get_degrees().end();
-        auto begin2 = second.get_degrees().begin();
-        auto end2 = second.get_degrees().end();
-        while (begin1 != end1 && begin2 != end2 &&
-                             begin1->first == begin2->first && begin1->second == begin2->second) {
-            ++begin1;
-            ++begin2;
+        const auto& [position1, position2] = std::mismatch(first.get_degrees().begin(), first.get_degrees().end(),
+                                                 second.get_degrees().begin(), second.get_degrees().end());
+        if(position2 == second.get_degrees().end()) {
+            return false;
         }
-        return Lex::compare(begin1, end1, begin2, end2);
-    }
-private:
-    using Iter = std::map<Monomial::IndexType, Monomial::DegreeType>::const_iterator;
-    static bool compare(Iter begin1, Iter end1, Iter begin2, Iter end2) {
-        if (begin1 != end1 && begin2 != end2 && (begin1->first > begin2->first
-                 || (begin1->first == begin2->first && begin1->second < begin2->second))) {
+        if( position1 == first.get_degrees().end()) {
             return true;
         }
-        if (begin1 == end1 && begin2 != end2) {
-            return true;
-        }
-        return false;
+        return *position1 < *position2;
     }
 
+    bool operator()(const Monomial& first, const Monomial& second) const {
+        return cmp(first, second);
+    }
 };
 
 class Deg {
 public:
     static bool cmp(const Monomial& first, const Monomial& second) {
-        return Deg::compare(first.get_degrees().begin(), first.get_degrees().end(),
-                                 second.get_degrees().begin(), second.get_degrees().end());
+        auto deg1 = total_degree(first.get_degrees().begin(), first.get_degrees().end());
+        auto deg2 = total_degree(second.get_degrees().begin(), second.get_degrees().end());
+        return deg1 < deg2;
     }
+
+    bool operator()(const Monomial& first, const Monomial& second) const {
+        return cmp(first, second);
+    }
+
 private:
     using Iter = std::map<Monomial::IndexType, Monomial::DegreeType>::const_iterator;
     static Monomial::DegreeType total_degree(Iter it1, Iter it2) {
@@ -46,12 +43,6 @@ private:
             ++it1;
         }
         return res;
-    }
-
-    static bool compare(Iter begin1, Iter end1, Iter begin2, Iter end2) {
-        Monomial::DegreeType deg1 = total_degree(begin1, end1);
-        Monomial::DegreeType deg2 = total_degree(begin2, end2);
-        return (deg1 < deg2);
     }
 };
 
@@ -77,6 +68,10 @@ public:
         if (T1::cmp(first, second)) return true;
         if (T1::cmp(second, first)) return false;
         return Sum<Tk...>::cmp(first, second);
+    }
+
+    bool operator()(const Monomial& first, const Monomial& second) const {
+        return cmp(first, second);
     }
 };
 

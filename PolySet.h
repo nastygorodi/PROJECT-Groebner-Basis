02d2ea32e9f244e07ab6a835138d4cf0f6c8144f
nastya <60066872+nastygorodi@users.noreply.github.com>
2021-04-15 12:23:06 +0300

    update and rename PolySet.h

---
+++ PolySet.h
@@ -0,0 +1,90 @@
+#pragma once
+#include <iostream>
+#include "Polynomial.h"
+#include "PolynomialOrder.h"
+#include <set>
+
+namespace Groebner {
+
+template <class Coeff, class Order>
+class PolySet {
+public:
+    using PolyContainer = std::set<Polynomial<Coeff, Order>, PolynomialOrder<Coeff, Order>>;
+    PolySet() = default;
+
+    PolySet(const Polynomial<Coeff, Order>& p) {
+        polynomials_.emplace(p);
+    }
+
+    PolySet(std::initializer_list<Polynomial<Coeff, Order>> polynomials) {
+        for (auto p : polynomials) {
+            if (p != 0) {
+                polynomials_.emplace(p);
+            }
+        }
+    }
+
+    PolySet(PolyContainer polynomials) {
+        polynomials_ = polynomials;
+        reduce();
+    }
+
+    const PolyContainer& get_polynomials() const {
+        return polynomials_;
+    }
+
+    void add(const Polynomial<Coeff, Order>& p) {
+        polynomials_.emplace(p);
+    }
+
+    void addSet(const PolySet<Coeff, Order> other) {
+        for (auto item : other.get_polynomials()) {
+            polynomials_.emplace(item);
+        }
+    }
+
+    void clear() {
+        polynomials_.clear();
+    }
+
+    void exclude(const Polynomial<Coeff, Order>& p) {
+        auto it = std::find_if(polynomials_.begin(), polynomials_.end(), [&p](const auto& x) {return x == p;});
+        if (it != polynomials_.end()) {
+            polynomials_.erase(it);
+        }
+    }
+
+    void normalize() {
+        auto it = polynomials_.begin();
+        while (it != polynomials_.end()) {
+            auto it_ = it;
+            ++it_;
+            auto tmp = polynomials_.extract(it);
+            tmp.value().norm();
+            polynomials_.insert(move(tmp));
+            it = it_;
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const PolySet<Coeff, Order>& current) {
+        for (const auto& p : current.polynomials_) {
+            out << p << " ; ";
+        }
+        return out;
+    }
+
+private:
+    void reduce() {
+        auto it = polynomials_.cbegin();
+        while (it != polynomials_.cend()) {
+            if (*it == Polynomial<Coeff, Order>(0)) {
+                it = polynomials_.erase(it);
+            } else {
+                ++it;
+            }
+        }
+    }
+    
+    PolyContainer polynomials_;
+};
+}

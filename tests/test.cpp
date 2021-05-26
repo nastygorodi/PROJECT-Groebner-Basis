227afe8a9b6e15efc633ea0ec0c7324bc88b024d
nastya <60066872+nastygorodi@users.noreply.github.com>
2021-05-27 01:17:16 +0300

    upd test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,11 @@
+#include "test_Algorithm.h"
 #include "test_Monomial.h"
-#include "test_Polynomial.h"
 #include "test_PolySet.h"
+#include "test_Polynomial.h"
 
 void run_all_tests() {
     test_Monomial::run();
     test_Polynomial::run();
     test_PolySet::run();
+    test_Algorithm::run();
 }

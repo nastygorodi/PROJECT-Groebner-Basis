58686d08756c4ef4301b03e577ae0d953030844a
nastya <60066872+nastygorodi@users.noreply.github.com>
2021-05-27 01:11:34 +0300

    upd Algorithm.h

--- Algorithm.h
+++ Algorithm.h
@@ -1,13 +1,14 @@
 #pragma once
 #include <iostream>
+
 #include "PolynomialSet.h"
 
 namespace Groebner {
 template <class Coeff, class Order>
 class Algorithm {
-public:
+ public:
     using TermsContainer = std::map<Monomial, Coeff, Reverse<Order>>;
-    static Monomial L(const Polynomial<Coeff, Order>& p) {
+    static Monomial L(const Polynomial<Coeff, Order> &p) {
         Monomial result;
         auto terms = p.get_terms();
         if (terms.begin() != terms.end()) {
@@ -16,7 +17,7 @@ public:
         return result;
     }
 
-    static Polynomial<Coeff, Order> S(const Polynomial<Coeff, Order>& f1, const Polynomial<Coeff, Order>& f2) {
+    static Polynomial<Coeff, Order> S(const Polynomial<Coeff, Order> &f1, const Polynomial<Coeff, Order> &f2) {
         auto m = Monomial::lcm(L(f1), L(f2));
         auto m1 = m / L(f1);
         auto m2 = m / L(f2);
@@ -24,34 +25,30 @@ public:
         return result;
     }
 
-    static void make_reduction(Polynomial<Coeff, Order>& g, const Polynomial<Coeff, Order>& f, typename std::map<Monomial, Coeff, Reverse<Order>>::const_iterator it) {
+    static void make_reduction(Polynomial<Coeff, Order> &g, const Polynomial<Coeff, Order> &f, typename std::map<Monomial, Coeff, Reverse<Order>>::const_iterator it) {
         auto m1 = it->first / L(f);
         g = f.coeff_of(L(f)) * g - it->second * (m1 * f);
     }
 
-    static std::pair<bool, typename std::map<Monomial, Coeff, Reverse<Order>>::const_iterator> reduction_check(const Polynomial<Coeff, Order>& g, const Polynomial<Coeff, Order>& f) {
-        auto it = g.get_terms().begin();
-        while ((it != g.get_terms().end()) && (!(it->first).is_divisible_by(L(f)))) {
-            ++it;
-        }
-
+    static std::pair<bool, typename std::map<Monomial, Coeff, Reverse<Order>>::const_iterator> reduction_check(const Polynomial<Coeff, Order> &g, const Polynomial<Coeff, Order> &f) {
+        auto it = std::find_if(g.get_terms().begin(), g.get_terms().end(), [&f](const auto &x) { return x.first.is_divisible_by(L(f)); });
         if (it == g.get_terms().end()) {
             return {false, it};
         }
         return {true, it};
     }
 
-    static void complete_reduction(Polynomial<Coeff, Order>& g, const Polynomial<Coeff, Order>& f) {
+    static void complete_reduction(Polynomial<Coeff, Order> &g, const Polynomial<Coeff, Order> &f) {
         while (reduction_check(g, f).first == true) {
             make_reduction(g, f, reduction_check(g, f).second);
         }
     }
 
-    static void complete_reduction_bySet(Polynomial<Coeff, Order>& g, const PolySet<Coeff, Order>& X) {
+    static void complete_reduction_bySet(Polynomial<Coeff, Order> &g, const PolySet<Coeff, Order> &X) {
         int count_reductions = 1;
         while (count_reductions != 0) {
             count_reductions = 0;
-            for (auto& f : X.get_polynomials()) {
+            for (auto &f : X.get_polynomials()) {
                 auto g_old = g;
                 complete_reduction(g, f);
                 if (g_old != g) {
@@ -61,7 +58,7 @@ public:
         }
     }
 
-    static void complete_reduction_SetBySet(PolySet<Coeff, Order>& Y, const PolySet<Coeff, Order>& X) {
+    static void complete_reduction_SetBySet(PolySet<Coeff, Order> &Y, const PolySet<Coeff, Order> &X) {
         auto y_container = Y.get_polynomials();
         auto it = y_container.begin();
         while (it != y_container.end()) {
@@ -78,7 +75,7 @@ public:
         Y = Y_new;
     }
 
-    static void autoreduction(PolySet<Coeff, Order>& X) {
+    static void autoreduction(PolySet<Coeff, Order> &X) {
         int count_reductions = 1;
         PolySet<Coeff, Order> Y;
         while (count_reductions != 0) {
@@ -89,7 +86,7 @@ public:
                 ++it_;
                 auto tmp_old = *it;
                 auto tmp = *it;
-                X.exclude(*it);
+                X.exclude(it);
                 complete_reduction_bySet(tmp, X);
                 complete_reduction_bySet(tmp, Y);
                 if (tmp != Coeff(0)) {
@@ -102,10 +99,9 @@ public:
             }
             std::swap(X, Y);
         }
-        X.normalize();
     }
 
-    static void make_pairsForTwo(const PolySet<Coeff, Order>& X, const PolySet<Coeff, Order>& Y, PolySet<Coeff, Order>& ans) {
+    static void make_pairsForTwo(const PolySet<Coeff, Order> &X, const PolySet<Coeff, Order> &Y, PolySet<Coeff, Order> &ans) {
         auto it1 = X.get_polynomials().begin();
         while (it1 != X.get_polynomials().end()) {
             auto f1 = *it1;
@@ -126,9 +122,10 @@ public:
             }
             ++it1;
         }
+        autoreduction(ans);
     }
 
-    static void make_pairs(const PolySet<Coeff, Order>& Y, PolySet<Coeff, Order>& ans) {
+    static void make_pairs(const PolySet<Coeff, Order> &Y, PolySet<Coeff, Order> &ans) {
         auto it1 = Y.get_polynomials().begin();
         while (it1 != Y.get_polynomials().end()) {
             auto f1 = *it1;
@@ -149,22 +146,23 @@ public:
             }
             ++it1;
         }
+        autoreduction(ans);
     }
 
-    static PolySet<Coeff, Order> make_GB(PolySet<Coeff, Order>& X) {
+    static PolySet<Coeff, Order> make_GB(PolySet<Coeff, Order> &X) {
         autoreduction(X);
         PolySet<Coeff, Order> Y, Z;
         make_pairs(X, Y);
         while (Y.get_polynomials().size() != 0) {
             Z.clear();
-            autoreduction(X);
             make_pairsForTwo(X, Y, Z);
             make_pairs(Y, Z);
             X.addSet(Y);
+            autoreduction(X);
             Y = Z;
         }
-        autoreduction(X);
+        X.normalize();
         return X;
     }
 };
-}
+}  // namespace Groebner

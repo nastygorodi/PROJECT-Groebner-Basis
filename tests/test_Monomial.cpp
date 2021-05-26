dea7c289f37bf1437a1b018118794170068bd452
nastya <60066872+nastygorodi@users.noreply.github.com>
2021-05-27 01:24:49 +0300

    upd test_Monomial.cpp

--- tests/test_Monomial.cpp
+++ tests/test_Monomial.cpp
@@ -1,15 +1,21 @@
-#include "../Monomial.h"
 #include <stdlib.h>
+
 #include <iostream>
+
+#include "../Monomial.h"
 
 using namespace Groebner;
 
 namespace test_Monomial {
 Monomial generate_Monomial(int test_id, int rand_) {
-    int num_var = rand() % rand_;
-    Monomial m = {{0, rand() % (rand_ + 10)}};
-    for (int i = 1; i < num_var; ++i) {
-        Monomial cur = {{i, rand() % (rand_ + 10)}};
+    int num_var = rand() % rand_ + 1;
+    Monomial m;
+    for (int i = 0; i < num_var; ++i) {
+        auto ind = rand() % rand_;
+        if (ind == 0) {
+            continue;
+        }
+        Monomial cur = {{i, ind}};
         m *= cur;
     }
     return m;
@@ -18,8 +24,8 @@ Monomial generate_Monomial(int test_id, int rand_) {
 void run() {
     std::srand(17);
     for (int test_id = 1; test_id < 100; ++test_id) {
-        auto m_1 = generate_Monomial(test_id + 30, test_id + 20);
-        auto m_2 = generate_Monomial(test_id + 20, test_id + 30);
+        auto m_1 = generate_Monomial(test_id + 17, test_id + 11);
+        auto m_2 = generate_Monomial(test_id + 11, test_id + 17);
         try {
             auto check_degree_of_variable = m_1.degree_of_variable(rand() % test_id);
             auto check_mult = m_1 * m_2;
@@ -30,9 +36,10 @@ void run() {
             auto check_lcm = Monomial::lcm(m_1, m_2);
         } catch (const std::exception& e) {
             std::cout << "Test: " << test_id << "\n";
-            std::cout << "m1 = " << m_1 << "\n" << "m2 = " << m_2 << "\n";
+            std::cout << "m1 = " << m_1 << "\n"
+                      << "m2 = " << m_2 << "\n";
         }
     }
     std::cout << "OK : all Monomial's tests;\n";
 }
-}
+}  // namespace test_Monomial
